35359dada40c31701155c99a0c1c38ff4817e664
Charles Callender <charlescallender.cc@gmail.com>
2022-02-03 19:11:47 -0800

    Updated server file to render homepage on heroku

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -76,7 +76,7 @@ db.once('open', () => {
 app.use(express.static(path.join(__dirname, '../whatsapp-mern/build')));
 
 // API routes
-app.get('/', (req,res) => {
+app.get('*', (req,res) => {
 res.status(200).send("Route OK");
 res.sendFile(path.join(__dirname, '../whatsapp-mern/build/index.html'));
 
